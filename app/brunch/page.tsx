358a7498ec13a173269cea2c07b39b826dc49ddd
aminaliaqat04 <aminaliaqat04@gmail.com>
2024-08-12 14:02:35 +0500

    minor fixes for mobile screen

--- app/brunch/page.tsx
+++ app/brunch/page.tsx
@@ -11,8 +11,8 @@ export default function page() {
       <div className="min-h-screen overflow-hidden relative flex justify-center items-center">
         <div className="absolute -z-10 bg-[--dark-color] opacity-40 h-full w-full"></div>
         <Image src={BannerBg} alt="" className="absolute object-cover inset-0 min-h-screen w-full -z-20" />
-        <h1 className="uppercase text-6xl text-[--light-color] font-bold tracking-widest text-center">Brunch Menu</h1>
-        <div className="h-48 bg-gradient-to-t from-[--dark-color] max-lg:from-70% to-transparent absolute lg:bottom-0 max-lg:bottom-20 w-full -z-10"></div>
+        <h1 className="uppercase max-lg:text-4xl lg:text-6xl text-[--light-color] font-bold tracking-widest text-center">Brunch Menu</h1>
+        <div className="h-48 bg-gradient-to-t from-[--dark-color] to-transparent absolute bottom-0 w-full -z-10"></div>
       </div>
       <div className="bg-[--dark-color] text-[--light-color] flex flex-col items-center gap-5 py-20 pt-32 lg:px-10 max-lg:px-5 z-10">
         <div className="border-4 border-[--light-color] outline-1 outline-offset-4 outline-[--light-color] outline px-10 py-20 w-full lg:w-2/3 mx-auto">
--- app/eat/page.tsx
+++ app/eat/page.tsx
@@ -9,10 +9,10 @@ export default function page() {
       <div className="min-h-screen overflow-hidden relative flex justify-center items-center">
         <div className="absolute -z-10 bg-[--dark-color] opacity-40 h-full w-full"></div>
         <Image src={BannerBg} alt="" className="absolute object-cover inset-0 min-h-screen w-full -z-20" />
-        <h1 className="uppercase text-6xl text-[--light-color] font-bold tracking-widest text-center">Carolina Pizzeria Menu</h1>
-        <div className="h-48 bg-gradient-to-t from-[--dark-color] max-lg:from-70% to-transparent absolute lg:bottom-0 max-lg:bottom-20 w-full -z-10"></div>
+        <h1 className="uppercase max-lg:text-4xl lg:text-6xl text-[--light-color] font-bold tracking-widest text-center px-10 !leading-relaxed">Carolina Pizzeria Menu</h1>
+        <div className="h-48 bg-gradient-to-t from-[--dark-color] max-lg:from-70% to-transparent absolute lg:bottom-0 max-lg:bottom-24 w-full -z-10"></div>
       </div>
-      <div className="bg-[--dark-color] text-[--light-color] flex flex-col items-center gap-5 py-20 lg:px-10 max-lg:px-5 max-lg:-mt-48 z-10">
+      <div className="bg-[--dark-color] text-[--light-color] flex flex-col items-center gap-5 py-20 lg:px-10 max-lg:px-5 max-lg:-mt-56 z-10">
         <p className="text-xl">Modern dishes that capture the flavors of the season</p>
         <MenuTabs />
       </div>
